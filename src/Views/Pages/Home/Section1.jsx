f8a5360e7f1bca7f9234170e4e87a2aebe76aaf6
Akshay <palakshay3091@gmail.com>
2025-02-26 22:15:22 +0530

    updatebyak

--- src/Views/Pages/Home/Section1.jsx
+++ src/Views/Pages/Home/Section1.jsx
@@ -3,6 +3,7 @@ import Header from '../../Layout/Header/Header'
 import BatmanBtn from '../../Layout/Component/BatmanBtn/BatmanBtn'
 import Lanyard from '../../Layout/Component/Lanyard/Lanyard'
 import DragItems from '../../Layout/Component/DragItems/DragItems';
+import FallingText from '../../Layout/Component/FallingText/FallingText';
 
 
 const Section1 = () => {
@@ -21,19 +22,28 @@ const Section1 = () => {
                                     <img src="firebaseicon.gif" className='ms-3 img-fluid' width={70} height={70} alt="" />
                                 </div>
                                 <DragItems />
-                            </div> */}
-                            <div className="container mt-0 mt-lg-5 py-0 py-lg-5">
-                                <p className='text-center fs-1' style={{ color: "black" }}>We design. We develop.</p>
+                            </div> */}      {/* <p className='text-center fs-1' style={{ color: "black" }}>We design. We develop.</p>
                                 <p className='text-center display-1 fw-bolder boldText'>We deliver your vision <br /> to the world</p>
                                 <p className='text-center fs-3 ' style={{ color: "black" }}>Explore our work and discover your potential.</p>
                                 <p className='text-center'>
                                     <BatmanBtn text={"Show Me The Magic"} />
-                                </p>
-                            </div>
+                                </p> */}
+
+                            <FallingText
+                                text={`We Design. We Develop. We Deliver your Vision to the World`}
+                                highlightWords={["Design", "Develop", "Deliver", "Vision", "Work"]}
+                                highlightClass="highlighted"
+                                trigger="hover"
+                                backgroundColor="transparent"
+                                wireframes={false}
+                                gravity={0.2}
+                                fontSize="3.5rem"
+                                mouseConstraintStiffness={0.9}
+                            />
                         </div>
                     </div>
                 </div>
-            </section>
+            </section >
         </>
     )
 }
---
+++ src/Views/Pages/Home/Section2.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Section2 = () => {
+    return (
+        <>
+            <section id='section2'>
+                <h1>hello akshay</h1>
+            </section>
+        </>
+    )
+}
+
+export default Section2
