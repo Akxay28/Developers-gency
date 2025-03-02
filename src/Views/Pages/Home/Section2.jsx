d1eb01c0bb4f0f3be0eac94b91ece7e324ad6a33
Dhaval Patel <rnwdhaval1@gmail.com>
2025-03-02 13:46:53 +0530

    changes

--- src/Views/Pages/Home/Section2.jsx
+++ src/Views/Pages/Home/Section2.jsx
@@ -2,9 +2,17 @@ import React, { useEffect, useRef } from 'react';
 import locomotiveScroll from 'locomotive-scroll';
 import gsap from 'gsap';
 import ScrollTrigger from 'gsap/ScrollTrigger';
+import Aos from 'aos';
 
 
-gsap.registerPlugin(ScrollTrigger);  // Register ScrollTrigger plugin
+gsap.registerPlugin(ScrollTrigger);
+
+Aos.init({
+    duration: 1000, // Animation duration in ms
+    easing: 'ease-in-out', // Animation easing
+    once: false, // Animation happens only once
+    offset: 100, // Trigger animation when 200px from the viewport
+});
 
 const Section2 = () => {
     const scrollref = useRef(null);
@@ -66,35 +74,44 @@ const Section2 = () => {
                 // toggleActions: 'play none none reverse',
             },
         });
-
-
     }, []);
 
     return (
         <section id='section2' ref={scrollref} className='section2 py-5'>
-            <div className="container">
-                <p className='mt-5 mb-5 py-5 text-center  display-1 Whatwedo text-light fw-bolder'>
+            <div className="container-fluid p-5">
+                <p
+                    className='mt-5 mb-5 py-5 text-center display-1 Whatwedo text-light fw-bolder'
+                >
                     What We Do
                 </p>
-                <div className="row mt-5 py-5  ">
-                    <div className="col-lg-4 ">
-                        <div className="container border" style={{ backgroundColor: '#f4cce9c5', borderRadius: '35px' }}>
-                            <p className=' display-3  fw-bolder'  >
+                <div className="row mt-5   py-3">
+                    <div className="col-lg-4 mt-5" data-aos="zoom-in" data-aos-duration="1000">
+                        <div
+                            className="container border  py-3"
+                            style={{ backgroundColor: '#f4cce9c5', borderRadius: '35px' }}
+                        >
+                            <p className='display-3 fw-bolder'>
                                 Design & Branding
                             </p>
-                            <p className='fs-3' style={{ color: "#DCD7C9" }}>From logo creation to full-scale branding strategies, we design visuals that captivate and make your brand impossible to ignore!</p>
+                            <p className='fs-3' style={{ color: "#DCD7C9" }}>
+                                From logo creation to full-scale branding strategies, we design visuals that captivate and make your brand impossible to ignore!
+                            </p>
                             <img src="designBranding.png" alt="" />
                         </div>
                     </div>
-                    <div className="col-lg-8 ">
-                        <div className="row d-flex flex-column">
-                            <div className="col-lg-12 ">
-                                <div className="container mb-5 border" style={{ backgroundColor: '#fcf951', borderRadius: '35px' }}>
+
+                    <div className="col-lg-8 mt-5" data-aos="zoom-in" data-aos-duration="1000">
+                        <div className="row d-flex flex-column py-5">
+                            <div className="col-lg-12" data-aos="zoom-in" data-aos-duration="1000">
+                                <div
+                                    className="container mb-5 border"
+                                    style={{ backgroundColor: '#fcf951', borderRadius: '35px' }}
+                                >
                                     <div className="row">
                                         <div className="col-lg-8 py-5">
-                                            <p className='   display-3 fw-bolder'>
-                                                Web
-                                                Development</p>
+                                            <p className='display-3 fw-bolder'>
+                                                Web Development
+                                            </p>
                                             <p>
                                                 We build websites and apps that don’t just look good but perform flawlessly. Our development services are tailored to deliver robust, scalable, and user-friendly solutions for businesses of all sizes. From concept to deployment, we’ve got you covered!
                                             </p>
@@ -104,35 +121,46 @@ const Section2 = () => {
                                         </div>
                                     </div>
                                 </div>
-                                <div className="row">
-                                    <div className="col-lg-6">
-                                        <div className="container  border" style={{ backgroundColor: '#f4cce9c5', borderRadius: '35px' }}>
-                                            <div className="row">
-                                                <div className="col-lg-6  py-2">
-                                                    <p className='display-5 fw-bolder'>
-                                                        UI/UX
-                                                    </p>
-                                                    <p>
-                                                        From Figma prototypes to design systems and micro interactions, we got you covered. Our UI/UX services are designed to create seamless user experiences that drive engagement and conversions.
-                                                    </p>
-                                                </div>
-                                                <div className="col-lg-6 py-5 ">
-                                                    <img src="uiux.png" alt="" className='img-fluid h-100 w-100' />
-                                                </div>
+                            </div>
+
+                            <div className="row">
+                                <div className="col-lg-6" data-aos="zoom-in" data-aos-duration="800">
+                                    <div
+                                        className="container border"
+                                        style={{ backgroundColor: '#2bc0e4', borderRadius: '35px' }}
+                                    >
+                                        <div className="row">
+                                            <div className="col-lg-6 py-3 mt-5">
+                                                <p className='display-5 fw-bolder'>
+                                                    UI/UX
+                                                </p>
+                                                <p style={{ color: '#EAECC6' }}>
+                                                    From Figma prototypes to design systems and micro interactions, we got you covered. Our UI/UX services are designed to create seamless user experiences that drive engagement and conversions.
+                                                </p>
+                                            </div>
+                                            <div className="col-lg-6 ">
+                                                <img src="uiux.png" alt="" className='img-fluid h-100 w-100' />
                                             </div>
                                         </div>
                                     </div>
-                                    <div className="col-lg-6">
-                                        <div className="container  border" style={{ backgroundColor: '#f4cce9c5', borderRadius: '35px' }}>
-                                            <div className="row">
-                                                <div className="col-lg-8 py-5">
-                                                    <p className='display-5 fw-bolder'>
-                                                        SEO </p>
+                                </div>
 
-                                                </div>
-                                                <div className="col-lg-4">
-                                                    <img src="webDev.png" alt="" />
-                                                </div>
+                                <div className="col-lg-6" data-aos="zoom-out" data-aos-duration="800">
+                                    <div
+                                        className="container border"
+                                        style={{ backgroundColor: '#06d69ed3', borderRadius: '35px' }}
+                                    >
+                                        <div className="row">
+                                            <div className="col-lg-6 py-3 mt-5">
+                                                <p className='display-5 fw-bolder text-light'>
+                                                    SEO
+                                                </p>
+                                                <p style={{ color: '#f8ffe5' }}>
+                                                    From Figma prototypes to design systems and micro interactions, we got you covered. Our UI/UX services are designed to create seamless user experiences that drive engagement and conversions.
+                                                </p>
+                                            </div>
+                                            <div className="col-lg-6 py-5">
+                                                <img src="seo.png" alt="" className='img-fluid h-100 w-100' />
                                             </div>
                                         </div>
                                     </div>
@@ -142,7 +170,7 @@ const Section2 = () => {
                     </div>
                 </div>
             </div>
-        </section >
+        </section>
     );
 };
 
