8f28a6146f15775ad4cde63cae0766664897df0a
Dhaval Patel <rnwdhaval1@gmail.com>
2025-03-05 18:39:55 +0530

    robot

--- src/Views/Pages/Home/Home.jsx
+++ src/Views/Pages/Home/Home.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import Section1 from './Section1'
 import Section2 from './Section2'
 import Section3 from './Section3'
+import Robot from './Robot'
 
 
 const Home = () => {
     return (
         <>
-            <Section1 />
+            <Robot />
+            {/* <Section1 /> */}
             <Section2 />
             <Section3 />
         </>
---
+++ src/Views/Pages/Home/Robot.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Spline from '@splinetool/react-spline';
+
+
+const Robot = () => {
+    return (
+        <>
+            <main id='section3' className='section3'>
+                <div id='circle'></div>
+                <Spline scene="https://prod.spline.design/TGzpheehwZBk86J7/scene.splinecode" />
+            </main>
+        </>
+    )
+}
+
+export default Robot
